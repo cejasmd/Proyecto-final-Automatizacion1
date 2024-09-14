bbfc132029298edd67aa0de7bd4883cbc43c5d9f
jafeth <jafethmd@hotmail.com>
2024-09-14 17:11:58 -0600

    edicion

--- tests/InicioSesion.spec.js
+++ tests/InicioSesion.spec.js
@@ -3,8 +3,8 @@ import {test, expect} from '@playwright/test';
 test('InicioSesion', async ({page}) => {
 
     await page.goto('https://www.saucedemo.com/.');
-    const username = await page.locator("#user-name");
     await page.waitForTimeout(1000);
+    const username = await page.locator("#user-name");
     await username.fill("standard_user");
     await page.waitForTimeout(1000);
     const pasword = await page.locator("#password");
@@ -13,6 +13,9 @@ test('InicioSesion', async ({page}) => {
     const login = await page.locator("#login-button");
     await login.click();
     await page.waitForTimeout(1000);
+
+
+
 
     //assertion
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
